77de32d4ca150070bffd2afd064b4b765ce5bdf5
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-03-12 19:05:15 +0000

    Create adjacencymatrix.h

---
+++ TAD/adjacencymatrix.h
@@ -0,0 +1,9 @@
+#ifndef ADJACENCYMATRIX_H
+#define ADJACENCYMATRIX_H
+
+void allocM(int ***m, int l, int c);
+void clearM(int **m, int l, int c);
+void printM(int **m, int l, int c);
+void freeM(int ***m, int l);
+
+#endif
