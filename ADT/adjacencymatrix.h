ffa8748453cf71772e6d2e9d053c1a8cb2287c79
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-03-18 17:35:15 -0300

    Update adjacencymatrix.h

--- ADT/adjacencymatrix.h
+++ ADT/adjacencymatrix.h
@@ -4,6 +4,9 @@
 void allocM(int ***m, int l, int c);
 void clearM(int **m, int l, int c);
 void printM(int **m, int l, int c);
+void printMAd(int **m, int l, int c);
+void lightedgeM(int **m, int v, char dg);
+void removeM(int **m, int vo, int vd);
 void freeM(int ***m, int l);
 
 #endif
