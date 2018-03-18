2063c9feb882d03eae8346a8ae2c1f720524f3a3
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-03-18 17:38:10 -0300

    Update linkedlist.h

--- ADT/linkedlist.h
+++ ADT/linkedlist.h
@@ -1,4 +1,18 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+typedef struct _node{
+	int v, w;
+	struct _node *link;
+}node;
+
+void allocL(node **l, int v);
+void clearL(node *l, int v);
+void createN(node *l, int p, int v, int w);
+void printL(node *l, int v);
+void printLAd(node *l, int p);
+void lightedgeL(node *l, int v, char dg);
+void removeL(node *l, int vo, int vd);
+void freeL(node *l, int v);
+
 #endif
