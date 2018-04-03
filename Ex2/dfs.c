72b2f257bc23b1e65b9154116afb31c29ddb2f37
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-03 00:04:20 -0300

    Update dfs.c

--- Ex2/dfs.c
+++ Ex2/dfs.c
@@ -3,8 +3,6 @@
 #include"../ADT/linkedlist.h"
 #include"../ADT/lifo.h"
 
-/*
-
 void readGraph(node *l, int e){
 	int vo = 0, vd = 0;
 	while(e--){
@@ -13,62 +11,73 @@ void readGraph(node *l, int e){
 	}
 }
 
-void bfs(node *l, fifo *queue, int v, int vo, int vd){
-	int i = 0, success = 0;
+void dfs(node *l, lifo *stack, int v, int vo, int vd){
+	int i = 0, success = 0, ed = 0, ok = 0;
+
+	lifo path;
+	createLIFO(&path);
 
 	int *c;//"color" array: #0 not yet visited #1 visited #2 ajdacents visited
 	int *p;//parents array
-	int *d;//distance from origin
+	int time = 0, *d, *f;//timestamps
 	c = (int *)malloc(sizeof(int)*v);
 	p = (int *)malloc(sizeof(int)*v);
 	d = (int *)malloc(sizeof(int)*v);
+	f = (int *)malloc(sizeof(int)*v);
 	for(i = 0; i < v; i++){
-		c[i] = 0; p[i] = -1; d[i] = -1;//clears
+		c[i] = 0; p[i] = -1; d[i] = -1; f[i] = -1;//clears
 	}
 
 	c[vo]++;//root node painted gray
-	d[vo] = 0;//its distance from root is 0
-	insertFIFO(vo, queue);//enqueue
-	while(queue->head){//while queue isnt empty
-		if(queue->head->data != vd){//if visiting node isnt the one we're looking for
-			nodeFIFO *u = queue->head;//create a pointer
-			adj adja = LAd(l, u->data);//find its ajdacents
-			for(i = 0; i < adja.q; i++){//do it for its adjacents
+	d[vo] = time++;
+	insertLIFO(vo, stack);//enqueue
+	while(stack->head){//while queue isnt empty
+		insertLIFO(stack->head->data, &path);//enqueue
+		if(stack->head->data != vd){//if visiting node isnt the one we're looking for
+			int u = stack->head->data;
+			removeLIFO(stack);
+			adj adja = LAd(l, u);//find its ajdacents
+			ed = 0;
+			ok = 0;
+			for(i = adja.q-1; i >= 0; i--){//do it for its adjacents
 				if(c[adja.aj[i]] == 0){//if this adjacent hasnt been visited
 					c[adja.aj[i]]++;//visit
-					d[adja.aj[i]] = d[u->data] + 1;//calculate its distance from root (his parent's distance + 1)
+					d[adja.aj[i]] = time++;
 					if(p[adja.aj[i]] == -1){//if his parent isnt yet set
-						p[adja.aj[i]] = u->data;//set his parent
+						p[adja.aj[i]] = u;//set his parent
 					}
-					insertFIFO(adja.aj[i], queue);//enqueue
+					insertLIFO(adja.aj[i], stack);//enqueue
+				}
+				else{
+					ed++;
+					if(adja.aj[i] == vd){
+						ok = 1;
+					}
 				}
 			}
+			if(adja.q == ed && !ok){
+				removeLIFO(&path);
+			}
 			free(adja.aj);
-			c[u->data]++;//visited all its adjacents, paint it black
-			removeFIFO(queue);//dequeue
+			c[u]++;//visited all its adjacents, paint it black
+			f[u] = time++;
 		}
 		else{//if we find the node we're looking for
 			success = 1;
-			while(queue->head)
-				removeFIFO(queue);//empty queue
+			while(stack->head)
+				removeLIFO(stack);
 		}
 	}
 
 	if(success){
-		int veri = vd, size = d[veri] + 1, *sol;//size of the solution array is the distance from the node to root set on the 'd' array
-		sol = (int *)calloc(size, sizeof(int));//solution array
-		sol[d[veri]] = vd;//set last value of the solution array as the desired destination vertex
-		veri = p[veri];//aux receives its parent
-		for(i = size-2; i > 0 && veri != vo && veri != -1; i--){//while aux isnt the origin vertex nor empty parent (-1)
-			sol[i] = veri;//store solution
-			veri = p[veri];//aux receives its parent
+		lifo path2;
+		createLIFO(&path2);
+		while(path.head){
+			insertLIFO(removeLIFO(&path), &path2);//enqueue
 		}
-		if(vo != vd)//if the origin vertex isnt the destination vertex
-			sol[0] = veri;//aux should now be orign vertex and must be on solution arrya
-		for(i = 0; i < size; i++){
-			printf("%d ", sol[i]);//print solution
+		while(path2.head){
+			printf("%d ", removeLIFO(&path2));//enqueue
 		}
-		free(sol);
 	}
 	printf("\n");
 
@@ -77,15 +86,10 @@ void bfs(node *l, fifo *queue, int v, int vo, int vd){
 	free(d);
 }
 
-*/
-
 int main(){
-
-/*
 
 	int v = 0, e = 0;
 	int op1 = 0, op2 = 0;
-	int i = 0;
 
 	node *l = NULL;
 	scanf("%d %d\n", &v, &e);//reads number of vertex and edges
@@ -93,16 +97,15 @@ int main(){
 
 	readGraph(l, e);//read graph input
 
-	fifo queue;
-	createFIFO(&queue);
+	lifo stack;
+	createLIFO(&stack);
 
 	while(scanf("%d %d\n", &op1, &op2) == 2){
-		bfs(l, &queue, v, op1, op2);
+		//printf("dfs(%d, %d)\n", op1, op2);
+		dfs(l, &stack, v, op1, op2);
 	}
 
 	freeL(l, v);//free linked list
-
-*/
 
 	return 0;
 }
