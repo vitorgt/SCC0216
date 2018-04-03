3622f2c3cdfe661a383c659b3d7fcc3ea1d94e14
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-03 00:07:17 -0300

    Update lifo.c

--- ADT/lifo.c
+++ ADT/lifo.c
@@ -16,31 +16,21 @@ void insertLIFO(lifotype data, lifo *a){
 }
 
 lifotype removeLIFO(lifo *a){
-	if(!(a->head))
+	if(!(a) || !(a->head)){
 		printf("Nothing to remove\n");
-	nodeLIFO *temp = a->head;
-	lifotype resu = a->head->data;
-	a->head = a->head->next;
-	free(temp);
-	return resu;
+	}
+	else{
+		nodeLIFO *temp = a->head;
+		lifotype resu = a->head->data;
+		a->head = a->head->next;
+		free(temp);
+		return resu;
+	}
 }
 
 void createLIFO(lifo *a){
 	a->head = NULL;
 	a->tail = NULL;
-}
-
-int isemptyLIFO(lifo a){
-	if(!(a.head)) return 1;
-	return 0;
-}
-
-lifotype topLIFO(lifo a){
-	if(!(a.head)){
-		printf("Nothing\n");
-		return;
-	}
-	return a.head->data;
 }
 
 void printLIFO(lifo a){
