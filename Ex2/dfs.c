08c7c27637eb631000c8de16506096e13f979267
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-04-03 18:37:20 -0300

    Update dfs.c

--- Ex2/dfs.c
+++ Ex2/dfs.c
@@ -28,108 +28,46 @@ void dfs(node *l, int ing, int *c, int *p, int *d, int *f, int *time){
 }
 
 void dfsI(node *l, int v, int vo, int vd){
-	int i = 0;
-
-	int *c;//"color" array: #0 not yet visited #1 visited #2 ajdacents visited
-	int *p;//parents array
-	int time = 0, *d, *f;//timestamps
-	c = (int *)malloc(sizeof(int)*v);
-	p = (int *)malloc(sizeof(int)*v);
-	d = (int *)malloc(sizeof(int)*v);
-	f = (int *)malloc(sizeof(int)*v);
-	for(i = 0; i < v; i++){
-		c[i] = 0; p[i] = -1; d[i] = -1; f[i] = -1;//clears
+	if(vo == vd){
+		printf("%d\n", vo);
 	}
+	else{
+		int i = 0;
 
-	dfs(l, vo, c, p, d, f, &time);
-
-	if(p[vd] != -1){
-		lifo path;
-		createLIFO(&path);
-		int veri = vd;
-		while(p[veri] != -1){
-			insertLIFO(veri, &path);//enqueue
-			veri = p[veri];
+		int *c;//"color" array: #0 not yet visited #1 visited #2 ajdacents visited
+		int *p;//parents array
+		int time = 0, *d, *f;//timestamps
+		c = (int *)malloc(sizeof(int)*v);
+		p = (int *)malloc(sizeof(int)*v);
+		d = (int *)malloc(sizeof(int)*v);
+		f = (int *)malloc(sizeof(int)*v);
+		for(i = 0; i < v; i++){
+			c[i] = 0; p[i] = -1; d[i] = -1; f[i] = -1;//clears
 		}
-		insertLIFO(veri, &path);//enqueue
-		while(path.head){
-			printf("%d ", removeLIFO(&path));//enqueue
+
+		dfs(l, vo, c, p, d, f, &time);
+
+		if(p[vd] != -1){
+			lifo path;
+			createLIFO(&path);
+			int veri = vd;
+			while(p[veri] != -1){
+				insertLIFO(veri, &path);//enqueue
+				veri = p[veri];
+			}
+			insertLIFO(veri, &path);//enqueue
+			while(path.head){
+				printf("%d ", removeLIFO(&path));//enqueue
+			}
 		}
 		printf("\n");
+
+		free(c);
+		free(p);
+		free(d);
+		free(f);
 	}
-
-	free(c);
-	free(p);
-	free(d);
-	free(f);
 }
-
-/*
-   void dfs(node *l, lifo *stack, int v, int vo, int vd){
-   int i = 0, success = 0, ed = 0, ok = 0;
-
-   lifo path;
-   createLIFO(&path);
-
-
-   c[vo]++;//root node painted gray
-   d[vo] = time++;
-   insertLIFO(vo, stack);//enqueue
-   while(stack->head){//while queue isnt empty
-   insertLIFO(stack->head->data, &path);//enqueue
-   if(stack->head->data != vd){//if visiting node isnt the one we're looking for
-   int u = stack->head->data;
-   removeLIFO(stack);
-   adj adja = LAd(l, u);//find its ajdacents
-   ed = 0;
-   ok = 0;
-   for(i = adja.q-1; i >= 0; i--){//do it for its adjacents
-   if(c[adja.aj[i]] == 0){//if this adjacent hasnt been visited
-   c[adja.aj[i]]++;//visit
-   d[adja.aj[i]] = time++;
-   if(p[adja.aj[i]] == -1){//if his parent isnt yet set
-   p[adja.aj[i]] = u;//set his parent
-   }
-   insertLIFO(adja.aj[i], stack);//enqueue
-   }
-   else{
-   ed++;
-   if(adja.aj[i] == vd){
-   ok = 1;
-   }
-   }
-   }
-   if(adja.q == ed && !ok){
-   removeLIFO(&path);
-   }
-   free(adja.aj);
-   c[u]++;//visited all its adjacents, paint it black
-   f[u] = time++;
-   }
-   else{//if we find the node we're looking for
-   success = 1;
-   while(stack->head)
-   removeLIFO(stack);
-   }
-   }
-
-   if(success){
-   lifo path2;
-   createLIFO(&path2);
-   while(path.head){
-   insertLIFO(removeLIFO(&path), &path2);//enqueue
-   }
-   while(path2.head){
-   printf("%d ", removeLIFO(&path2));//enqueue
-   }
-   }
-   printf("\n");
-
-   free(c);
-   free(p);
-   free(d);
-   }
- */
 
 int main(){
 
@@ -141,9 +79,6 @@ int main(){
 	allocL(&l, v);//allocates and clears a linked list
 
 	readGraph(l, e);//read graph input
-
-	lifo stack;
-	createLIFO(&stack);
 
 	while(scanf("%d %d\n", &op1, &op2) == 2){
 		//printf("dfs(%d, %d)\n", op1, op2);
