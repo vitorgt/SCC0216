66b3dffdb458290c6d22f0516f6d11155177995b
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2018-05-16 19:52:10 -0300

    Update mst.c

--- Ex4/mst.c
+++ Ex4/mst.c
@@ -12,13 +12,14 @@ void readGraph(node *l, int e){
 	while(e--){
 		scanf("%d %d %d", &vo, &vd, &w);
 		createN(l, vo, vd, w);
+		//createN(l, vd, vo, w);
 	}
 }
 
 void mst(node *l, int v, int vo){//vo := origin vertex
 	fifoP prio;
 	createFIFOP(&prio);
-	int i = 0;
+	int i = 0, count = 0;
 	int *c = NULL;//"color" array: #0 not yet visited #1 visited #2 ajdacents visited
 	int *p = NULL;//parents array
 	int *d = NULL;//distance from origin
@@ -30,13 +31,43 @@ void mst(node *l, int v, int vo){//vo := origin vertex
 		insertFIFOP(createFIFOPnode(i, d[i]), &prio);
 	}
 
-	printFIFOP(prio);
-
 	c[vo]++;//root node painted gray
 	d[vo] = 0;//its distance from root is 0
 	updateFIFOPnode(createFIFOPnode(vo, d[vo]), &prio);
-
-	printFIFOP(prio);
+	while(!isemptyFIFOP(prio)){
+		printFIFOP(prio);
+		fifopriotype u = removeFIFOP(&prio);
+		printf("visitando %d\n", u.v);
+		c[u.v] = count++;
+		adj adja = LAd(l, u.v);//find its ajdacents
+		for(i = 0; i < adja.q; i++){
+			printf("aj%d de %d\n", adja.aj[i], u.v);
+			int cost = mapFIFOP(adja.aj[i], prio);
+			int weig = weight(l, u.v, adja.aj[i]);
+			printf("w%d c%d\n", weig, cost);
+			if(weig < cost){
+				updateFIFOPnode(createFIFOPnode(adja.aj[i], weight(l, u.v, adja.aj[i])), &prio);
+				p[adja.aj[i]] = u.v;
+			}
+		}
+	}
+	for(i = 0; i < v; i++){//0 1 2 3 4	//0 1 4 2 3
+		printf("%d ", c[i]);
+	}
+	printf("\n");
+	for(i = 0; i < v; i++){//-1 0 1 0 0	//-1 0 0 1 3
+		printf("%d ", p[i]);
+	}
+	printf("\n");
+	for(i = 1; i < v; i++){
+		int j = 0;
+		for(j = 1; j < v; j++){
+			if(c[j] == i){
+				printf("(%d,%d) ", p[j], j);
+			}
+		}
+	}
+	printf("\n");
 }
 
 int main(){
